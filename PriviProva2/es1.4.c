58722da4a209c2a067a103c8fe8928810d661d52
Privitorta <125799472+Privitorta@users.noreply.github.com>
2025-07-31 23:41:08 +0200

    correzione

---
+++ PriviProva2/es1.4.c
@@ -0,0 +1,28 @@
+/*------------------------------------------------------------------------------------
+ES 1.4
+Scrivere un sottoprogramma genera che visualizza tutti i numeri binari rappresentati
+da una stringa costituita dai valori 0, 1 e x, dove le x possono assumere valore sia
+0 che 1. Quindi, se il sottoprogramma riceve in ingresso la stringa 1x0, visualizza
+100 e 110 (l'ordine non è importante). +1 punto bonus se la soluzione implementata
+è ricorsiva.
+
+ESEMPIO
+Ingresso: 
+1x01x1x
+Uscita: 
+1101111
+1101110
+1101011
+1101010
+1001111
+1001110
+1001011
+1001010
+
+Suggerimento: Potrebbe essere utile sviluppare una funzione "binario" che riceve
+un intero (e altri valori necessari) e restituisce una stringa contenente la codifica
+binaria corrispondente, ad esempio binario(7, ...) -> 110
+------------------------------------------------------------------------------------*/
+
+
+
